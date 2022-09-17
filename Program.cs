40660e0f201ab2cb626abcf287191b8aea6afb60
saulkaique <saul.silva@etec.sp.gov.br>
2022-09-17 10:24:41 -0300

    bom terceiro a ser entregue ja pronto e sem erros

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("--Nome--Ninja:");
+
+Console.WriteLine("Digite seu Nome:");
+string nome = Console.ReadLine()!;
+
+
+string NomeNinja = nome.Trim().ToLower()
+            .Replace("a", "Ka")
+            .Replace("b", "ZU")
+            .Replace("c", "MI")
+            .Replace("d", "TE")
+            .Replace("e", "KU")
+            .Replace("f", "LU")
+            .Replace("g", "JI")
+            .Replace("h", "RI")
+            .Replace("i", "KI")
+            .Replace("j", "ZUS")
+            .Replace("k", "ME")
+            .Replace("l", "TA")
+            .Replace("m", "RIN")
+            .Replace("n", "TO")
+            .Replace("o", "MO")
+            .Replace("p", "NO")
+            .Replace("q", "KE")
+            .Replace("r", "SHI")
+            .Replace("s", "ARI")
+            .Replace("t", "CHI")
+            .Replace("u", "DO")
+            .Replace("v", "RU")
+            .Replace("w", "MEI")
+            .Replace("x", "NA")
+            .Replace("y", "FU")
+            .Replace("z", "ZI");
+
+NomeNinja =  $"{NomeNinja.Substring(0,1).ToUpper()}{NomeNinja.Substring(1).ToLower()}";           
+
+Console.Write($"\nSeu Nome Ninja È:{NomeNinja}.");
